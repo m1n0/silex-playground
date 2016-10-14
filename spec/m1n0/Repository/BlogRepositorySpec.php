291db69de8df34c162817fcadef9b4499fd4368a
m1n0 <mino.lukac@gmail.com>
2016-10-14 16:08:31 +0200

    Adjust blog spec to use the new db behind it.

--- spec/m1n0/Repository/BlogRepositorySpec.php
+++ spec/m1n0/Repository/BlogRepositorySpec.php
@@ -3,6 +3,7 @@
 namespace spec\m1n0\Repository;
 
 use Doctrine\DBAL\Connection;
+use m1n0\Entity\Blog;
 use m1n0\Repository\BlogRepository;
 use PhpSpec\ObjectBehavior;
 
@@ -22,18 +23,15 @@ class BlogRepositorySpec extends ObjectBehavior
         // Prepare data and mocks.
         $sql = "SELECT * FROM posts WHERE id = ?";
 
-        $testResult = new \stdClass();
-        $testResult->title = 'Using Silex';
-        $testResult->body = 'Lorem Ipsum';
-        $db->fetchAssoc($sql, [0])->willReturn($testResult);
+        $testResult1 = new Blog(1, 1476364162, 1, 'Using Silex', 'Lorem Ipsum');
+        $db->fetchAssoc($sql, [0])->willReturn($testResult1);
 
-        $testResult->title = 'Learning Silex';
-        $testResult->body = 'Dolor sit amet';
-        $db->fetchAssoc($sql, [1])->willReturn($testResult);
+        $testResult2 = new Blog(2, 1476104961, 2, 'Learning Silex', 'Dolor sit amet');
+        $db->fetchAssoc($sql, [1])->willReturn($testResult2);
 
         // Act.
-        $this->get(0)->shouldReturn($testResult);
-        $this->get(1)->shouldReturn($testResult);
+        $this->get(0)->shouldReturn($testResult1);
+        $this->get(1)->shouldReturn($testResult2);
     }
 
     function it_throws_not_found()
