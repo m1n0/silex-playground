0a3487eaef57fd00feb3a8b75ee847672f25aea3
m1n0 <mino.lukac@gmail.com>
2016-10-19 21:58:32 +0200

    Simple spec for user repository, just an example what it could do.

---
+++ spec/m1n0/Repository/UserRepositorySpec.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace spec\m1n0\Repository;
+
+use Doctrine\DBAL\Connection;
+use m1n0\Entity\User;
+use m1n0\Repository\UserRepository;
+use PhpSpec\ObjectBehavior;
+
+class UserRepositorySpec extends ObjectBehavior
+{
+    function let(Connection $db)
+    {
+        $this->beConstructedWith($db);
+    }
+
+    function it_is_initializable()
+    {
+        $this->shouldHaveType(UserRepository::class);
+    }
+
+    function it_returns_user_given_existing_id(Connection $db)
+    {
+        // Prepare data and mocks.
+        $query = "Select * from users where id = ?";
+
+        $result = ['id' => 1, 'name' => 'admin', 'mail' => 'admin@example.com'];
+        $testResultUser = new User($result['id'], $result['name'], $result['mail']);
+        $db->fetchAssoc($query, [1])->willReturn($result);
+
+        // Act.
+        $this->getUserById(1)->shouldReturn($testResultUser);
+    }
+}
